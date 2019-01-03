ba2c05e34b55bf0c8adcbffb99bb1c817be17e31
Ewgraf <ewgraf@mail.ru>
2019-01-03 18:16:58 +0300

    initial

---
+++ CoubSharper.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CoubSharper {
+	public class CoubClient : IDisposable {
+		private const string AuthorizeAppUri = "http://coub.com/oauth/authorize";
+		private const string RequestAccessTokenUri = "http://coub.com/oauth/token";
+		private readonly HttpClient _client;
+		private readonly string _applicationId;
+		private readonly string _secret;
+		private readonly string _callbackUri;
+
+		private string _code;
+		private CoubToken _token;
+
+		public CoubClient() {
+			_client = new HttpClient();
+		}
+
+		public CoubClient(string applicationId, string secret, string callbackUri) : base() {
+			_applicationId = applicationId;
+			_secret = secret;
+			_callbackUri = callbackUri;
+		}
+
+		public void GetCode() {
+			// http://coub.com/oauth/authorize?response_type=code&client_id=eac6f6491031535222d9553440d7048b7f6401a914aedb1303c5a4991840f92f&redirect_uri=http%3A%2F%2Fyourapp.com%2Fauth%2Fcallback&scope=like+recoub
+			string authUri = $"{AuthorizeAppUri}"
+						   + $"?redirect_uri={_callbackUri}" // the callback Uri which the Coub server  responses in with client credentials.
+						   + $"&client_id={_applicationId}" // the unique application identifier
+						   + $"&response_type=code"; // should be set to 'code'
+						   // By  default,  the  request  will  be   send   for logged_in access mode.
+						   // To request any additional modes you need  to  add scope parameter to the URL.
+						   // All needed additional modes should  be  set  with the "+" sign separator:
+						   //+ $"&scope=like+recoub"
+
+			OpenBrowser(authUri);
+
+			using (Socket server = new Socket(SocketType.Stream, ProtocolType.Tcp)) {
+				server.Bind(new IPEndPoint(IPAddress.Any, 80));
+				server.Listen(1);
+				// 128 is enough to get first line of request
+				byte[] buffer = new byte[128];
+				using (Socket connection = server.Accept()) {
+					connection.Receive(buffer);
+					connection.Close();
+					connection.Dispose();
+				}
+				string request = Encoding.ASCII.GetString(buffer);
+				// parsing first line 'GET /?code=aed2926c3ede9de608cb359844eae54b815910b55576b15db3fbe545d45d77cf HTTP/1.1'
+				_code = request.Split(Environment.NewLine.ToCharArray())[0].Split(' ')[1].Replace("/?code=", string.Empty);
+			}
+		}
+
+		public void GetToken() {
+			string uri = $"{RequestAccessTokenUri}"
+					   + $"?client_id={_applicationId}" // the unique application identifier
+					   + $"&client_secret={_secret}" // the app's secret token
+					   + $"&code={_code}" // the received authorization code
+					   + $"&redirect_uri={_callbackUri}" // the callback URL which the Coub server responses in with client credentials
+					   + $"&grant_type=authorization_code"; // should be set to 'authorization_code'
+			HttpResponseMessage response = _client.PostAsync(uri, null).Result;
+			string content = response.Content.ReadAsStringAsync().Result;
+			_token = JsonConvert.DeserializeObject<CoubToken>(content);
+		}
+
+		public CoubsSearchResponse SearchCoubs(string query, OrderBy orderBy = OrderBy.newest_popular, int page = 1) {
+			// GET /api/v2/search/coubs?q=best coub ever&order_by=likes_count&page=1
+			string uri = "http://coub.com/api/v2/search/coubs"
+					  + $"?q={query}"
+					  + $"&order_by={orderBy}"
+					  + $"&page={page}"; // the number of the page containing results (by default is set to 1)
+			return JsonConvert.DeserializeObject<CoubsSearchResponse>(
+				_client.GetAsync(uri).Result.Content.ReadAsStringAsync().Result
+			);
+		}
+
+		// credits to: 'https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/'
+		private static void OpenBrowser(string url) {
+			try {
+				Process.Start(url);
+			} catch {
+				// hack because of this: https://github.com/dotnet/corefx/issues/10361
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+					url = url.Replace("&", "^&");
+					Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+				} else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+					Process.Start("xdg-open", url);
+				} else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+					Process.Start("open", url);
+				} else {
+					throw;
+				}
+			}
+		}
+
+		#region IDisposable
+
+		private bool _disposed = false;
+
+		~CoubClient() => Dispose(false);
+
+		public void Dispose() {
+			Dispose(true);
+			// This object will be cleaned up by the Dispose method. Therefore,
+			// you should call GC.SupressFinalize to take this object  off  the
+			// finalization queue and prevent finalization code for this object
+			// from executing a second time.
+			GC.SuppressFinalize(this);
+		}
+
+		// Dispose(bool disposing)  executes  in  two  distinct  scenarios.  If
+		// disposing equals true,  the  method  has  been  called  directly  or
+		// indirectly by a user's code. Managed and unmanaged resources can  be
+		// disposed. If disposing equals false, the method has been  called  by
+		// the runtime from inside the finalizer and you should  not  reference
+		// other objects. Only unmanaged resources can be disposed.
+		protected virtual void Dispose(bool disposing) {
+			if (!_disposed) {
+				if (disposing) {
+					// Dispose managed resources.
+					_client.Dispose();
+				}
+
+				// Dispose unmanaged resources.
+				//Kernel32.CloseHandle(handle);
+				//handle = IntPtr.Zero;
+
+				_disposed = true;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Entities.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoubSharper {
+	public enum OrderBy { // lower_dash-case for Uris
+		likes_count,
+		views_count,
+		newest,
+		oldest,
+		newest_popular
+	}
+
+	public class AccessModes {
+		public string LoggedIn = "logged_in"; // the default access mode
+		public string Create = "create"; // with the ability to create coub videos
+		public string Like = "like"; // with the ability to do a like on coub videos
+		public string Recoub = "recoub"; // with the ability to recoub coub videos
+		public string Follow = "follow"; // with the ability to follow channels
+		public string ChannelEdit = "channel_edit"; // with the ability to edit channels
+	}
+
+	public class CoubToken
+	{
+		public string access_token { get; set; }
+		public string token_type { get; set; }
+		public int expires_in { get; set; }
+		public string scope { get; set; }
+		public int created_at { get; set; }
+	}
+
+	public class Med
+	{
+		public string url { get; set; }
+		public int size { get; set; }
+	}
+
+	public class High
+	{
+		public string url { get; set; }
+		public int size { get; set; }
+	}
+
+	public class Video
+	{
+		public Med med { get; set; }
+		public High high { get; set; }
+	}
+
+	public class High2
+	{
+		public string url { get; set; }
+		public int size { get; set; }
+	}
+
+	public class Med2
+	{
+		public string url { get; set; }
+		public int size { get; set; }
+	}
+
+	public class Audio
+	{
+		public High2 high { get; set; }
+		public Med2 med { get; set; }
+		public double? sample_duration { get; set; }
+	}
+
+	public class Html5
+	{
+		public Video video { get; set; }
+		public Audio audio { get; set; }
+	}
+
+	public class Mobile
+	{
+		public string gifv { get; set; }
+		public List<string> audio { get; set; }
+	}
+
+	public class FileVersions
+	{
+		public Html5 html5 { get; set; }
+		public Mobile mobile { get; set; }
+	}
+
+	public class Chunks
+	{
+		public string template { get; set; }
+		public List<string> versions { get; set; }
+		public List<int> chunks { get; set; }
+	}
+
+	public class AudioVersions
+	{
+		public string template { get; set; }
+		public List<string> versions { get; set; }
+		public Chunks chunks { get; set; }
+	}
+
+	public class ImageVersions
+	{
+		public string template { get; set; }
+		public List<string> versions { get; set; }
+	}
+
+	public class FirstFrameVersions
+	{
+		public string template { get; set; }
+		public List<string> versions { get; set; }
+	}
+
+	public class Dimensions
+	{
+		public List<int> big { get; set; }
+		public List<int> med { get; set; }
+	}
+
+	public class ExternalDownload
+	{
+		public string type { get; set; }
+		public string service_name { get; set; }
+		public string url { get; set; }
+		public bool has_embed { get; set; }
+	}
+
+	public class AvatarVersions
+	{
+		public string template { get; set; }
+		public List<string> versions { get; set; }
+	}
+
+	public class Channel
+	{
+		public int id { get; set; }
+		public string permalink { get; set; }
+		public string title { get; set; }
+		public object description { get; set; }
+		public int followers_count { get; set; }
+		public int following_count { get; set; }
+		public AvatarVersions avatar_versions { get; set; }
+	}
+
+	public class Meta
+	{
+		public string service { get; set; }
+		public string duration { get; set; }
+	}
+
+	public class ExternalRawVideo
+	{
+		public int id { get; set; }
+		public string title { get; set; }
+		public string url { get; set; }
+		public string image { get; set; }
+		public string image_retina { get; set; }
+		public Meta meta { get; set; }
+		public double duration { get; set; }
+		public int raw_video_id { get; set; }
+		public bool has_embed { get; set; }
+	}
+
+	public class Meta2
+	{
+		public string service { get; set; }
+		public string duration { get; set; }
+	}
+
+	public class ExternalVideo
+	{
+		public int id { get; set; }
+		public string title { get; set; }
+		public string url { get; set; }
+		public string image { get; set; }
+		public string image_retina { get; set; }
+		public Meta2 meta { get; set; }
+		public double duration { get; set; }
+		public int raw_video_id { get; set; }
+		public bool has_embed { get; set; }
+	}
+
+	public class MediaBlocks
+	{
+		public List<object> uploaded_raw_videos { get; set; }
+		public List<ExternalRawVideo> external_raw_videos { get; set; }
+		public List<object> remixed_from_coubs { get; set; }
+		public ExternalVideo external_video { get; set; }
+	}
+
+	public class EditorialInfo
+	{
+	}
+
+	public class Tags
+	{
+		public int id { get; set; }
+		public string title { get; set; }
+		public string value { get; set; }
+	}
+
+	public class Category
+	{
+		public string big_image_url { get; set; }
+		public int id { get; set; }
+		public string med_image_url { get; set; }
+		public string permalink { get; set; }
+		public string small_image_url { get; set; }
+		public int subscriptions_count { get; set; }
+		public string title { get; set; }
+		public bool visible { get; set; }
+	}
+
+	public class Coub
+	{
+		public object flag { get; set; }
+		public object abuses { get; set; }
+		public object recoubs_by_users_channels { get; set; }
+		public bool favourite { get; set; }
+		public object recoub { get; set; }
+		public object like { get; set; }
+		public bool in_my_best2015 { get; set; }
+		public int id { get; set; }
+		public string type { get; set; }
+		public string permalink { get; set; }
+		public string title { get; set; }
+		public string visibility_type { get; set; }
+		public string original_visibility_type { get; set; }
+		public int channel_id { get; set; }
+		public DateTime created_at { get; set; }
+		public DateTime updated_at { get; set; }
+		public bool is_done { get; set; }
+		public int views_count { get; set; }
+		public object cotd { get; set; }
+		public object cotd_at { get; set; }
+		public bool published { get; set; }
+		public DateTime published_at { get; set; }
+		public bool reversed { get; set; }
+		public bool from_editor_v2 { get; set; }
+		public bool is_editable { get; set; }
+		public bool original_sound { get; set; }
+		public bool has_sound { get; set; }
+		public object recoub_to { get; set; }
+		public FileVersions file_versions { get; set; }
+		public AudioVersions audio_versions { get; set; }
+		public ImageVersions image_versions { get; set; }
+		public FirstFrameVersions first_frame_versions { get; set; }
+		public Dimensions dimensions { get; set; }
+		public List<int> site_w_h { get; set; }
+		public List<int> page_w_h { get; set; }
+		public List<int> site_w_h_small { get; set; }
+		public bool age_restricted { get; set; }
+		public bool age_restricted_by_admin { get; set; }
+		public bool? not_safe_for_work { get; set; }
+		public bool allow_reuse { get; set; }
+		public bool dont_crop { get; set; }
+		public bool banned { get; set; }
+		public bool? global_safe { get; set; }
+		public string audio_file_url { get; set; }
+		public ExternalDownload external_download { get; set; }
+		public object application { get; set; }
+		public Channel channel { get; set; }
+		public object file { get; set; }
+		public string picture { get; set; }
+		public string timeline_picture { get; set; }
+		public string small_picture { get; set; }
+		public object sharing_picture { get; set; }
+		public int percent_done { get; set; }
+		public Tags[] tags { get; set; }
+		public Category[] categories { get; set; }
+		public int recoubs_count { get; set; }
+		public int remixes_count { get; set; }
+		public int likes_count { get; set; }
+		public int raw_video_id { get; set; }
+		public bool uploaded_by_ios_app { get; set; }
+		public bool uploaded_by_android_app { get; set; }
+		public MediaBlocks media_blocks { get; set; }
+		public string raw_video_thumbnail_url { get; set; }
+		public string raw_video_title { get; set; }
+		public bool video_block_banned { get; set; }
+		public double duration { get; set; }
+		public bool promo_winner { get; set; }
+		public object promo_winner_recoubers { get; set; }
+		public EditorialInfo editorial_info { get; set; }
+		public object promo_hint { get; set; }
+		public object beeline_best_2014 { get; set; }
+		public bool from_web_editor { get; set; }
+		public bool normalize_sound { get; set; }
+		public bool best2015_addable { get; set; }
+		public object ahmad_promo { get; set; }
+		public object promo_data { get; set; }
+		public object audio_copyright_claim { get; set; }
+		public bool? ads_disabled { get; set; }
+		public bool? is_safe_for_ads { get; set; }
+	}
+
+	public class CoubsSearchResponse
+	{
+		public string page { get; set; }
+		public int total_pages { get; set; }
+		public int per_page { get; set; }
+		public List<Coub> coubs { get; set; }
+	}
+}
